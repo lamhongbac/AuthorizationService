11848dbf0c3df171ff3737ad8d2269029e42df5f
lam hong bac <lamhong.bac@gmail.com>
2023-10-31 11:52:55 +0700

    Update

--- AuthorizationService/Data/UserInfo.cs
+++ AuthorizationService/Data/UserInfo.cs
@@ -2,5 +2,13 @@
 {
     public class UserInfo
     {
+        public int AppID { get; set; }
+        public int CompanyID { get; set; }
+        public string ID { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Roles { get; set; }
+        public Dictionary<string, object> CustomsClaims { get; set; }
     }
 }
