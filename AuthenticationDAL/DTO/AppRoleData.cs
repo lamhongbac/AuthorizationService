0f288a4ebadebf44e7a25c28de50764b2105081b
phamhoang.dat <phamhoang.dat@ms-apptech.com>
2023-11-01 12:01:26 +0700

    update code

---
+++ AuthenticationDAL/DTO/AppRoleData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthenticationDAL.DTO
+{
+    public class AppRoleData
+    {
+        public AppRoleData()
+        {
+            AppRoleUI = new AppRoleUI();
+            RoleRightUIs = new List<RoleRightUI>();
+        }
+        public AppRoleUI AppRoleUI { get; set; }
+        public List<RoleRightUI> RoleRightUIs { get; set; }
+    }
+}
