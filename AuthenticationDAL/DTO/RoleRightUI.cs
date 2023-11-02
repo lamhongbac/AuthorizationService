9a4b6f89c84061fb958b85df87b8bef55fbd4c63
phamhoang.dat <phamhoang.dat@ms-apptech.com>
2023-11-02 15:23:19 +0700

    update code

--- AuthenticationDAL/DTO/RoleRightUI.cs
+++ AuthenticationDAL/DTO/RoleRightUI.cs
@@ -4,7 +4,7 @@ using System;
 namespace AuthenticationDAL.DTO
 {
     [Table("RoleRights")]
-    public class RoleRightUI
+    public class RoleRightUI:BaseUI
     {
         [Key]
         public int Id { get; set; }
@@ -14,7 +14,7 @@ namespace AuthenticationDAL.DTO
         public bool CanRead { get; set; }
         public bool CanUpdate { get; set; }
         public bool CanDelete { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public string CreatedBy { get; set; }
+        //public DateTime CreatedOn { get; set; }
+        //public string CreatedBy { get; set; }
     }
 }
