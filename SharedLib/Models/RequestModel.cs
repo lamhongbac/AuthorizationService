6e6b10064bd73eb1a54273a19eddb764c1c693e7
phamhoang.dat <phamhoang.dat@ms-apptech.com>
2023-11-03 14:48:29 +0700

    update code

--- SharedLib/Models/RequestModel.cs
+++ SharedLib/Models/RequestModel.cs
@@ -6,6 +6,11 @@ namespace SharedLib.Models
 {
     public class RequestModel
     {
+        public RequestModel()
+        {
+            ID = 0;
+            Number = " ";
+        }
         public int ID { get; set; }
         public string Number { get; set; }
     }
