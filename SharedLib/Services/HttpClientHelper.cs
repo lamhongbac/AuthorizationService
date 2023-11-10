a66c66f4d00a15c796c66d23a46ed1f03d2d237b
phamhoang.dat <phamhoang.dat@ms-apptech.com>
2023-11-10 13:24:04 +0700

    update code

--- SharedLib/Services/HttpClientHelper.cs
+++ SharedLib/Services/HttpClientHelper.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SharedLib.Services.ClientHelper
+namespace SharedLib.Services
 {
     public interface IHttpClientHelper<T>
     {
