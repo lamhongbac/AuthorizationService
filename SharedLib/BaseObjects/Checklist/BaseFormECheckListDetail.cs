aac78fb465a4f5b578d31a60d3799d277ae0481e
huahoangnghinhthu <huahoangnghinhthu@ms-apptech.com>
2023-11-14 22:15:23 +0700

    update code

--- SharedLib/BaseObjects/Checklist/BaseFormECheckListDetail.cs
+++ SharedLib/BaseObjects/Checklist/BaseFormECheckListDetail.cs
@@ -7,8 +7,8 @@ namespace SharedLib.BaseObjects.Checklist
         public Guid ID { get; set; }
         public Guid ParentID { get; set; }
         public string Number { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public string EContent { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
