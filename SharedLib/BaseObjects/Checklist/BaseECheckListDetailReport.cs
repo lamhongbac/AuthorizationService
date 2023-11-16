0e69573bb334231bd95a689a764c2b599c1afce8
huahoangnghinhthu <huahoangnghinhthu@ms-apptech.com>
2023-11-16 17:09:52 +0700

    update BaseECheckListReport

--- SharedLib/BaseObjects/Checklist/BaseECheckListDetailReport.cs
+++ SharedLib/BaseObjects/Checklist/BaseECheckListDetailReport.cs
@@ -10,10 +10,10 @@ namespace SharedLib.BaseObjects.Checklist
         public Guid FormDetailID { get; set; }
         public bool IsDone { get; set; }
         public string Note { get; set; }
-        public string Image { get; set; }
+        public string? Image { get; set; }
         public string Number { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
         public string ModifiedBy { get; set; }
--- SharedLib/BaseObjects/Checklist/BaseECheckListReport.cs
+++ SharedLib/BaseObjects/Checklist/BaseECheckListReport.cs
@@ -13,14 +13,14 @@ namespace SharedLib.BaseObjects.Checklist
         public Guid FormID { get; set; }
         public string Number { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int UserID { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
         public string ModifiedBy { get; set; }
         public DateTime ModifiedOn { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public string Status { get; set; }
         public List<BaseECheckListDetailReport> ECheckListDetailReports { get; set; }
     }
