53801fc4901540325373480e8ed90f4a48c0a2e2
huahoangnghinhthu <huahoangnghinhthu@ms-apptech.com>
2023-11-17 09:36:15 +0700

    update ECheckListReportRequestModel

--- SharedLib/Models/ECheckListReportRequestModel.cs
+++ SharedLib/Models/ECheckListReportRequestModel.cs
@@ -1,11 +1,14 @@
-﻿namespace SharedLib.Models
+﻿using System;
+
+namespace SharedLib.Models
 {
     public class ECheckListReportRequestModel : RequestModel
     {
         public ECheckListReportRequestModel()
         {
-
+            FormID = Guid.NewGuid();
         }
         public int UserID { get; set; }
+        public Guid? FormID { get; set; }
     }
 }
