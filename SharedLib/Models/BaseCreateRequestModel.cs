7cb8b014bfe41d258ca1c42355f50beb85df9325
phamhoang.dat <phamhoang.dat@ms-apptech.com>
2023-11-21 17:17:38 +0700

    update code

--- SharedLib/Models/BaseCreateRequestModel.cs
+++ SharedLib/Models/BaseCreateRequestModel.cs
@@ -2,5 +2,6 @@
 {
     public class BaseCreateRequestModel : RequestModel
     {
+        public object? Data { get; set; }
     }
 }
--- SharedLib/Models/BaseUpdateRequestModel.cs
+++ SharedLib/Models/BaseUpdateRequestModel.cs
@@ -2,5 +2,6 @@
 {
     public class BaseUpdateRequestModel : RequestModel
     {
+        public object? Data { get; set; }
     }
 }
