11230114e6cdf41fb188673deb7d17344d689c9d
phamhoang.dat <phamhoang.dat@ms-apptech.com>
2023-11-30 11:54:17 +0700

    update code

--- SharedLib/BaseObjects/Checklist/BaseQAReviewSubDetailReport.cs
+++ SharedLib/BaseObjects/Checklist/BaseQAReviewSubDetailReport.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace SharedLib.BaseObjects.Checklist
 {
-    internal class BaseQAReviewSubDetailReport
+    public class BaseQAReviewSubDetailReport
     {
+        public Guid ID { get; set; }
+        public Guid QASubReportID { get; set; }
+        public Guid ReviewReportID { get; set; }
+        public bool IsAvailable { get; set; }
+        public string Code { get; set; }
+        public bool IsPass { get; set; }
+        public bool IsCCP { get; set; }
+        public bool IsRisk { get; set; }
+        public string Note { get; set; }
+        public string Images { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public decimal Point { get; set; }
     }
 }
