8874d389aed3fb1384bc2fe224299ed4564e2d62
phamhoang.dat <phamhoang.dat@ms-apptech.com>
2023-11-30 11:53:42 +0700

    update code

---
+++ SharedLib/BaseObjects/Checklist/BaseQAReviewSubDetailReport.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLib.BaseObjects.Checklist
+{
+    internal class BaseQAReviewSubDetailReport
+    {
+    }
+}
