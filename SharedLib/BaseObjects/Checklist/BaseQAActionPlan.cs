96c2f00428ddec59662d50f7fadd74a1aa3305e4
huahoangnghinhthu <huahoangnghinhthu@ms-apptech.com>
2023-12-01 14:49:16 +0700

    update BaseQAActionPlan

--- SharedLib/BaseObjects/Checklist/BaseQAActionPlan.cs
+++ SharedLib/BaseObjects/Checklist/BaseQAActionPlan.cs
@@ -19,7 +19,7 @@ namespace SharedLib.BaseObjects.Checklist
         public string Description { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
-        public string ModifedBy { get; set; }
+        public string ModifiedBy { get; set; }
         public DateTime ModifiedOn { get; set; }
         public List<BaseQAActionPlanDetail> QAActionPlanDetails { get; set; }
     }
--- SharedLib/BaseObjects/Checklist/BaseQAActionPlanDetail.cs
+++ SharedLib/BaseObjects/Checklist/BaseQAActionPlanDetail.cs
@@ -10,7 +10,7 @@ namespace SharedLib.BaseObjects.Checklist
         public string Description { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
-        public string ModifedBy { get; set; }
+        public string ModifiedBy { get; set; }
         public DateTime ModifiedOn { get; set; }
         public Guid ParentID { get; set; }
         public string PlanContent { get; set; }
