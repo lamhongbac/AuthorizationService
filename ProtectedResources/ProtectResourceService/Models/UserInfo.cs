3a0d6e8809578a33d032d54f5bb7de2254ff031d
AppTech Director <lamhong.bac@gmail.com>
2024-01-06 11:56:26 +0700

    updatecode

--- ProtectedResources/ProtectResourceService/Models/UserInfo.cs
+++ ProtectedResources/ProtectResourceService/Models/UserInfo.cs
@@ -3,37 +3,37 @@ using System.Security.Claims;
 
 namespace StudyApiAuth.Models
 {
-    public class UserInfo
-    {
-        public UserInfo()
-        {
-            ObjectRights = new Dictionary<string, List<string>>();
-            Roles = new List<string>();
-        }
-        public string ID { get; set; }
-        public string UserName { get; set; }
-        public string FullName { get; set; }
-        public string EmailAddress { get; set; }
-        public List<string> Roles { get; set; }
+    //public class UserInfo
+    //{
+    //    public UserInfo()
+    //    {
+    //        ObjectRights = new Dictionary<string, List<string>>();
+    //        Roles = new List<string>();
+    //    }
+    //    public string ID { get; set; }
+    //    public string UserName { get; set; }
+    //    public string FullName { get; set; }
+    //    public string EmailAddress { get; set; }
+    //    public List<string> Roles { get; set; }
 
-        //objectName;right1,right2
-        public Dictionary<string, List<string>> ObjectRights { get; set; }
+    //    //objectName;right1,right2
+    //    public Dictionary<string, List<string>> ObjectRights { get; set; }
 
-        public int AppID { get; set; }
-        public int CompanyID { get; set; }
-    }
-    public class LoginInfo
-    {
-        public LoginInfo()
-        {
-            LoginDate = DateTime.Now;
-            JwtData = new JwtData();
+    //    public int AppID { get; set; }
+    //    public int CompanyID { get; set; }
+    //}
+    //public class LoginInfo
+    //{
+    //    public LoginInfo()
+    //    {
+    //        LoginDate = DateTime.Now;
+    //        JwtData = new JwtData();
 
-        }
+    //    }
 
-        public DateTime LoginDate { get; set; }
-        //example cho truong hop user data tong quat duoi dang string object
+    //    public DateTime LoginDate { get; set; }
+    //    //example cho truong hop user data tong quat duoi dang string object
 
-        public JwtData JwtData { get; set; }
-    }
+    //    public JwtData JwtData { get; set; }
+    //}
 }
