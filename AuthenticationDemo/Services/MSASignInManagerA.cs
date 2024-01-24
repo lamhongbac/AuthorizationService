3d68c5cfe20d5fd8dd3e72fd5c76e7d27fe57933
IT Director <lamhong.bac@gmail.com>
2024-01-24 09:55:02 +0700

    Updatecode

--- AuthenticationDemo/Services/MSASignInManagerA.cs
+++ AuthenticationDemo/Services/MSASignInManagerA.cs
@@ -7,23 +7,24 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System.Reflection;
 using System.Security.Claims;
 using System.Security.Principal;
 
 namespace AuthenticationDemo.Services
 {
-    public  class MSASignInManagerA 
+    public class MSASignInManagerA
     {
-        
+
         IHttpContextAccessor _contextAccessor;
-      static  MSAUserInfo _userInfo;
+        static MSAUserInfo _userInfo;
         public MSASignInManagerA(IHttpContextAccessor contextAccessor)
         {
-            _contextAccessor= contextAccessor;
-           
+            _contextAccessor = contextAccessor;
+
         }
-        
+
         public bool IsSignedIn()
         {
             //return base.IsSignedIn(principal);
@@ -43,21 +44,23 @@ namespace AuthenticationDemo.Services
         }
         /// <summary>
         /// kiem tra role co trong logined user
+        /// 
         /// </summary>
-        /// <param name="strRoles">role set tren view</param>
+        /// <param name="strRoles">role set tren view:
+        /// HasPermission("about;read,list")</param>
         /// <returns></returns>
-        public  bool HasPermission(string strRoles)
+        public bool HasPermission(string strRoles)
         {
             List<string> roles = strRoles.Split(";").ToList();
-            string objectName = roles[0]; //company
-            List<string> required=roles[1].Split(",").ToList();
+            string objectName = roles[0]; //company,about
+            List<string> requiredRoles = roles[1].Split(",").ToList();
 
             //gia su sau khi check ra perm cua user la 
-            List<string> userRights= new List<string>() { "admin", "user" };
+            List<string> userRights = GetUserObjectRights(objectName);
             bool authorized = false;
-            foreach (string right in userRights)
+            foreach (string userRight in userRights)
             {
-                if (required.Contains(right))
+                if (requiredRoles.Contains(userRight))
                 {
                     authorized = true;
                     break;
@@ -65,10 +68,43 @@ namespace AuthenticationDemo.Services
             }
             return authorized;
         }
-        public  async Task SignInAsync(MSAUserInfo userInfo)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objectName"></param>
+        /// <returns></returns>
+        private List<string> GetUserObjectRights(string objectName)
+        {
+            List<string> objectPermissions = new List<string>();
+            var claims = _contextAccessor.HttpContext.User.Claims.FirstOrDefault(x => x.Type.ToLower() == "objectrights");
+
+            if (claims == null)
+            {
+                return objectPermissions;
+            }
+            string claimsValue = claims.Value;
+            Dictionary<string, List<string>> user_permissions = new Dictionary<string, List<string>>();
+            if (!string.IsNullOrEmpty(claimsValue))
+            {
+                user_permissions = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(claimsValue);
+                //userInfo.ObjectRights = user_permissions;
+                if (user_permissions != null && user_permissions.ContainsKey(objectName))
+                    objectPermissions = user_permissions[objectName];
+
+            }
+
+            return objectPermissions;
+
+
+
+
+            //return new List<string>() { "admin", "user" };
+        }
+
+        public async Task SignInAsync(MSAUserInfo userInfo)
         {
             if (_userInfo == null)
-                _userInfo= new MSAUserInfo();
+                _userInfo = new MSAUserInfo();
 
             _userInfo = userInfo;
 
@@ -102,7 +138,7 @@ namespace AuthenticationDemo.Services
         }
 
 
-      
+
         /// <summary>
         /// luu them thong tin vao session ngoai thong tin dc luu boi framework
         /// _httpContextAccessor.HttpContext
@@ -134,6 +170,7 @@ namespace AuthenticationDemo.Services
         /// <returns></returns>
         private async Task<bool> SetLogout()
         {
+            _userInfo = null;
             await _contextAccessor.HttpContext.SignOutAsync();
             _contextAccessor.HttpContext.Session.Remove(AppConstants.LoginDate);
             _contextAccessor.HttpContext.Session.Remove(AppConstants.JwtData);
@@ -171,6 +208,7 @@ namespace AuthenticationDemo.Services
         public async Task SignOutAsync()
         {
             await SetLogout();
+           
         }
     }
 }
